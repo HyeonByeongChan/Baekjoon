a1e7d042982f4c6967ac25e6ab9a68ebe4fc9ffd
현병찬 <104262694+HyeonByeongChan@users.noreply.github.com>
2022-06-19 16:03:46 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String [] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int y;
+		
+		y = in.nextInt();
+		
+		if(y%400 == 0 || y%4==0 && y%100!=0) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+	}
+}
